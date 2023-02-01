5a4902b86d39d965fb7b81811e7b901ccc27b564
Ivan Morado <melodious@im-silicon-magic-box.mynetworksettings.com>
2023-02-01 10:50:20 -0500

    basic vector geometry library

---
+++ src/vector_geo.py
@@ -0,0 +1,13 @@
+"""
+A python library for vector geometry operations
+Author: Ivan Morado
+Date: 02/01/2023
+"""
+
+# pass scalars to an object that will store it in vector form for 4 dimensions
+def vector_obj():
+    pass
+
+# pass vector obj and perform vector operation
+def vector_op():
+    pass
